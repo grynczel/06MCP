145b984ce2c0c285e5908e2cfe1b35bd5ae733fd
grynczel <grynczel.wojciech@gmail.com>
2015-02-11 17:04:04 +0100

    2 test en plus

--- Travail 1 - CT39/src/Main.java
+++ Travail 1 - CT39/src/Main.java
@@ -1,23 +1,29 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int[] vector 	= { -4, -5, 3, 7, -5, 100, -100, -100 }; //Max 105
-		int[] vector2 	= { 2, 3, -2, -1, 10 }; //Max 12
-		int[] vector3	= { -1,-5,-26}; // Max -1 or 0
-		int[] vector4	= {}; //IllegalArgumentException
-		
+		int[] vector = { -4, -5, 3, 7, -5, 100, -100, -100 }; // Max 105
+		int[] vector2 = { 2, 3, -2, -1, 10 }; // Max 12
+		int[] vector3 = { -1, -5, -26 }; // -1 or 0
+		int[] vector5 = { -1, -2, -3, -4, 0 }; // 0
+		int[] vector6 = { 20,-5,-10, -6, 1 }; // 0
+
 		System.out.println("Version 1");
 		System.out.println(CT39V106.maxSum(vector));
 		System.out.println(CT39V106.maxSum(vector2));
 		System.out.println(CT39V106.maxSum(vector3));
-		//System.out.println(CT39V106.maxSum(vector4));
+		// System.out.println(CT39V106.maxSum(vector4));
+		System.out.println(CT39V106.maxSum(vector5));
+		System.out.println(CT39V106.maxSum(vector6));
 
-		
+
 		System.out.println("Version 2");
 		System.out.println(CT39V206.maxSum(vector));
 		System.out.println(CT39V206.maxSum(vector2));
 		System.out.println(CT39V206.maxSum(vector3));
-		//System.out.println(CT39V206.maxSum(vector4));
+		// System.out.println(CT39V206.maxSum(vector4));
+		System.out.println(CT39V206.maxSum(vector5));
+		System.out.println(CT39V206.maxSum(vector6));
+
 	}
 
 }
