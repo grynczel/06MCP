33f4fe54341193bb5485d0cc11c8796d15bd7622
marievi <marievi@users.noreply.github.com>
2015-02-11 20:28:29 +0100

    Update CT39V106.java
    Modif réponses aux questions

--- Travail 1 - CT39/src/CT39V106.java
+++ Travail 1 - CT39/src/CT39V106.java
@@ -1,17 +1,17 @@
-//Ce fichier est encodÃ© en UTF8.
+//Ce fichier est encodÃƒÂ© en UTF8.
 
 //Grynczel Wojciech
 //Russello Helena
 //Visschers Marie
 //Wenders Audrey
 
-// Remplacez les deux caractÃ¨res XX par votre numÃ©ro de groupe encodÃ© sur deux chiffres!
+// Remplacez les deux caractÃƒÂ¨res XX par votre numÃƒÂ©ro de groupe encodÃƒÂ© sur deux chiffres!
 public class CT39V106 {
 
-	// Le paramÃ¨tre t n'est pas null.
-	// De plus, il reprÃ©sente un vecteur non vide de n entiers relatifs.
+	// Le paramÃƒÂ¨tre t n'est pas null.
+	// De plus, il reprÃƒÂ©sente un vecteur non vide de n entiers relatifs.
 
-	// Cette mÃ©thode ne modifie pas les Ã©lÃ©ments du tableau t.
+	// Cette mÃƒÂ©thode ne modifie pas les ÃƒÂ©lÃƒÂ©ments du tableau t.
 	// Elle renvoie la somme de la sous-sequence non vide maximum de t.
 	public static long maxSum(int[] t) {
 		long max = t[0];
@@ -30,49 +30,50 @@ public class CT39V106 {
 	// ***********************************************************************************
 	// Expliquez la notion de "programme correct"
 	/*
-	 * Pour savoir si un programme est correct, une question primordiale à se
+	 * Pour savoir si un programme est correct, une question primordiale Ã  se
 	 * poser est : "Mon programme fait-il bien ce que je voudrais qu'il fasse ?"
 	 * 
-	 * Il faut donc vérifier que le programme fasse ce que j'attend de lui et
-	 * pas autre chose. Pour ce faire, je dois définir ses spécifications. Les
-	 * spécifications font références a une suite de conditions que le programme
-	 * doit remplir : les données en entrées, les données en sortie, la gestion
-	 * des cas d'erreurs,… Elles permettent d'analyser le comportement et la
-	 * conception du programme grâce à des outils de raisonnement rigoureux qui
-	 * déduisent des informations utiles. Ces spécifications sont formels,
-	 * c'est-à-dire qu'elles ont une syntaxe particulière et une sémantique.
+	 * Il faut donc vÃ©rifier que le programme fasse ce que j'attend de lui et
+	 * pas autre chose. Pour ce faire, je dois dÃ©finir ses spÃ©cifications. Les
+	 * spÃ©cifications font rÃ©fÃ©rences a une suite de conditions que le programme
+	 * doit remplir : les donnÃ©es en entrÃ©es, les donnÃ©es en sortie, la gestion
+	 * des cas d'erreurs,â€¦ Elles permettent d'analyser le comportement et la
+	 * conception du programme grÃ¢ce Ã  des outils de raisonnement rigoureux qui
+	 * dÃ©duisent des informations utiles. Ces spÃ©cifications sont formelles,
+	 * c'est-Ã -dire qu'elles ont une syntaxe particuliÃ¨re et une sÃ©mantique.
 	 */
 	// ***********************************************************************************
-	// Expliquez comment vous avez dÃ©veloppÃ© ce programme
+	// Expliquez comment vous avez dÃƒÂ©veloppÃƒÂ© ce programme
 	/*
-	 * Nous avons besoin de deux variables pour résoudre ce problème : une
+	 * Nous avons besoin de deux variables pour rÃ©soudre ce problÃ¨me : une
 	 * variable qui enregistre la somme maximum (long max) et une autre qui
-	 * contient la valeur maximum après l'ajout de la valeur suivante dans le
-	 * tableau (long sum). Si "sum" est supérieure à "max", ça signifie qu'il
+	 * contient la valeur maximum aprÃ¨s l'ajout de la valeur suivante dans le
+	 * tableau (long sum). Si "sum" est supÃ©rieure Ã  "max", Ã§a signifie qu'il
 	 * s'agit de la nouvelle valeur maximale donc "max" prend cette valeur;
-	 * sinon, "max" reste inchangée. Dès que "sum" contient une somme négative,
-	 * celle-ci est remise à zéro. (En effet, une somme commençant par un
-	 * chiffre négatif ne sera jamais plus intéressante qu’une somme commençant
-	 * par 0.) La différence entre la version 1 (sous-séquences vide non
-	 * acceptées) et la version 2 (sous-séquences vide acceptées) est
-	 * l'initialisation des variables au début du programme : max contient
-	 * d'abord le premier élément du tableau dans la version 1 et zéro dans la
+	 * sinon, "max" reste inchangÃ©e. DÃ¨s que "sum" contient une somme nÃ©gative,
+	 * celle-ci est remise Ã  zÃ©ro. (En effet, une somme commenÃ§ant par un
+	 * chiffre nÃ©gatif ne sera jamais plus intÃ©ressante quâ€™une somme commenÃ§ant
+	 * par 0.) La diffÃ©rence entre la version 1 (sous-sÃ©quences vide non
+	 * acceptÃ©es) et la version 2 (sous-sÃ©quences vide acceptÃ©es) est
+	 * l'initialisation des variables au dÃ©but du programme : max contient
+	 * d'abord le premier Ã©lÃ©ment du tableau dans la version 1 et zÃ©ro dans la
 	 * version 2. En effet, si aucune valeur positive n'existe dans le tableau,
-	 * la version 2 affichera zéro.
+	 * la version 2 affichera zÃ©ro et la version 1 affichera le plus petit nombre nÃ©gatif.
 	 */
 
 	// ***********************************************************************************
-	// Expliquez pourquoi vous pensez que votre mÃ©thode est correcte
+	// Expliquez pourquoi vous pensez que votre mÃƒÂ©thode est correcte
 	/*
-	 * Nous avons fait plusieurs tests afin de s’assurer que notre méthode est
+	 * Notre mÃ©thode respecte les spÃ©cifications de la mÃ©thode ce qui nous permet de penser que celle-ci est correcte.
+	 * De plus, nous avons fait plusieurs tests afin de nous assurer que la mÃ©thode maxSum est
 	 * correcte. 
-	 * Voici quelques exemples : 
-	 * Que des nombres négatifs [-1, -5, -26, -18] : 
-	 * 		la version 1 retourne la valeur -1 tandis que la version 2 retourne 0. 
-	 * Que des nombres positifs [1, 5, 10, 65] : 
-	 * 		les 2 versions retournent la valeur 81 
-	 * Des nombres négatifs et des positifs [-4, -5, 3, 7, -5, 100, -100, -100] : 
-	 * 		les 2 versions retournent la valeur 105
-	 * En plus de ça elle respecte toutes les spécifications.
+	 * Voici quelques exemples des tests rÃ©alisÃ©s: 
+	 * Le tableau en paramÃ¨tre ne possÃ¨de que des nombres nÃ©gatifs [-1, -5, -26, -18] : 
+	 * 		la mÃ©thode retourne la valeur -1 tandis que la version 2 retourne 0. 
+	 * Le tableau en paramÃ¨tre ne possÃ¨de que des nombres positifs [1, 5, 10, 65] : 
+	 * 		la mÃ©thode retourne la valeur 81 
+	 * Le tableau en paramÃ¨tre ne possÃ¨de des nombres nÃ©gatifs et des positifs [-4, -5, 3, 7, -5, 100, -100, -100] : 
+	 * 		la mÃ©thode retourne la valeur 105
+	 * 
 	 */
 }
